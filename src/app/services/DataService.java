54b7f36a35ef99fde6c5564579514d51d1d22cf2
OleksandrChaika13 <axelrod1347@gmail.com>
2023-07-27 22:10:05 +0300

    Fixing "DataService" class

--- src/app/services/DataService.java
+++ src/app/services/DataService.java
@@ -7,8 +7,9 @@ import java.util.List;
 public class DataService {
     // Алгоритм лінійного пошуку
     public int search(List<Product> list, double x) {
-        for (int i = 0; i < list.; i++) {
-            if (list.get(i) == x) return i;
+        for (int i = 0; i < list.size(); i++) {
+            Product product = list.get(i);
+            if (product.getPrice() == x) return i;
         }
         return -1;
     }
